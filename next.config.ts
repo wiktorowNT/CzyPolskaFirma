8bb94ae4a47ee4fe2b08f45528a9afb2633d48ca
wiktorowNT <wiktorow123@gmail.com>
2025-08-14 16:28:25 +0200

    Wyłączono sprawdzanie błędów TypeScript na czas budowy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // --- DODAJ TEN FRAGMENT ---
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // --- KONIEC FRAGMENTU ---
 };
 
 export default nextConfig;
